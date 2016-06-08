09e5887fba93df2ade986710a10cacd54665fa19
ruochen_j <ruochenj@sfu.ca>
2016-06-07 21:39:12 -0700

    demo draft

--- crowddata.py
+++ crowddata.py
@@ -79,8 +79,8 @@ class CrowdData:
         return self
 
     def __del__(self):
-            self.cur.close()
-            self.cddb.close()
+        self.cur.close()
+        self.cddb.close()
 
 if __name__ == "__main__":
     data = ['http://farm4.static.flickr.com/3114/2524849923_1c191ef42e.jpg', 'http://www.7-star-admiral.com/0015_animals/0629_angora_hamster_clipart.jpg']

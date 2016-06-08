be992be38659858bfec22abb7e647b6ab9842325
ruochen_j <ruochenj@sfu.ca>
2016-06-07 21:27:43 -0700

    demo draft

--- crowddata.py
+++ crowddata.py
@@ -1,47 +1,90 @@
+import pbclient
+import sqlite3
+import requests
+import time
 
 class CrowdData:
-    cd = {"colname":"a list of values"} 
-    cd_name = # The name of the CrowdData object 
+    def __init__(self, endpoint, api_key, data, presenter, crowd_data_name = "crowdbase",
+                short_name = "short name", description = "description"):
+        self.cd_name = crowd_data_name
+        self.cd = {'data':data}
+        self.presenter = presenter
+        self.endpoint = endpoint
+        pbclient.set('endpoint', endpoint)
+        pbclient.set('api_key', api_key)
+        self.cddb = sqlite3.connect('crowddata.db')
+        self.cur = self.cddb.cursor()
+        exe_str = "CREATE TABLE " + self.cd_name + " (id integer PRIMARY KEY AUTOINCREMENT, data varchar(100))"
+        self.cur.execute(exe_str)
+        for d in self.cd['data']:
+            exe_str = "INSERT INTO " + self.cd_name + "(data) VALUES(?)"
+            self.cur.execute(exe_str, (d,))
+            self.cddb.commit()
+        p = pbclient.create_project(crowd_data_name, short_name, description)
+        self.project_id = p.id
+        p.info['task_presenter'] = open(presenter).read()
+        pbclient.update_project(p)
 
-    def __init__(self, data, "crowd_data_name"):
-        """
-            Here are a list of things to do in this function:
-            1. self.cd_name = crowd_data_name
-            2. cd["data"] = data
-            3. initialize a table called "crowd_data_name" in the sqlite database. In the table, there are two columns and the types of them are string.
-               They can be throught of as the hash key and the coresponding value. They serve as a cache to store the values (e.g., task ids and task results) returned from the crowdsourcing platform. 
-        """
+    def createTask(self, task_col = "task_col", n_answers = 30, priority_0 = 0, quorum = 0):
+        try:
+            exe_str = "ALTER TABLE " + self.cd_name + " ADD COLUMN " + task_col + " integer DEFAULT 0"
+            self.cur.execute(exe_str)
+            self.cddb.commit()
+        except:
+            print "Some tasks have already been created"
+        exe_str = "SELECT * FROM " + self.cd_name
+        self.cur.execute(exe_str)
+        data = self.cur.fetchall()
+        self.cd[task_col] = []
+        for d in data:
+            if d[2] == 0:
+                task_info = {'url_b': d[1]}
+                task = pbclient.create_task(self.project_id, task_info, n_answers, priority_0, quorum)
+                exe_str = "UPDATE " + self.cd_name + " SET " + task_col + "=? WHERE id=?"
+                print exe_str
+                print task.id
+                print d[0]
+                self.cur.execute(exe_str, (task.id, d[0],))
+                self.cddb.commit()
+                self.cd[task_col].append(task.id)
+        return self
 
-    def createTask(self, "data_col", presenter, "task_col"):
-        """
-            Here are a list of things to do in this function:
-            1. check if task_col has already existed in the cache. If yes, read it from the database
-            2. check if there are still some tasks that are not created. If yes, 
-              2.1 create them and update the database (i.e., inserting hashkey:"task_col" and hashvalue:json.dump(task_ids) into the cache)
-              2.2 cd["task_col"] = the list of task ids
-            3. return self for chaining
-        """
+    def getTaskResult(self, task_col = "task_col", result_col = "result_col"):
+        try:
+            exe_str = "ALTER TABLE " + self.cd_name + " ADD COLUMN " + result_col + " text DEFAULT NULL"
+            self.cur.execute(exe_str)
+            self.cddb.commit()
+        except:
+            print "Some tasks have existing results"
+        exe_str = "SELECT * FROM " + self.cd_name
+        self.cur.execute(exe_str)
+        data = self.cur.fetchall()
+        self.cd[result_col] = []
+        for d in data:
+            if d[3] == None:
+                url = self.endpoint + "/project/" + self.cd_name + "/" + str(d[2]) + "/results.json"
+                print url
+                result = requests.get(url)
+                while result.status_code != 200:
+                    print "waiting for contribution"
+                    time.sleep(60)
+                    result = requests.get(url)
+                result_json = result.json()
+                exe_str = "UPDATE " + self.cd_name + " SET " + result_col + "=? WHERE id=?"
+                self.cur.execute(exe_str, (str(result_json), d[0],))
+                self.cddb.commit()
+                self.cd[result_col].append(result_json)
+            else:
+                self.cd[result_col].append(eval(d[3]))
+        return self
 
-    def getTaskResult(self, "task_col", "result_col"):
-        """
-            task_col: This should be an existing column in the crowd_data. It represents the col name of the task id
-            result_col: This is the newly added column. It represents the col name that you want to write the result to 
-
-            Here are a list of things to do in this function:
-            1. check if result_col has already existed in the cache. If yes, read if from the database 
-            2 check if there are still some tasks whose results are not collected. If yes, 
-              2.1 wait for their results and update the database (i.e., inserting hashkey:"result_col" and hashvalue:json.dump(task_results) into the cache)
-              2.2 cd["result_col"] = the list of task results
-            3. return self for chaining
-        """
-
-
-    def __def__(self):
-        """
-            close the connection of database
-        """
+    def __del__(self):
+            self.cur.close()
+            self.cddb.close()
 
 if __name__ == "__main__":
-    data = [("image1.jpg"), ("image2.jpg")]
-    cd = CrowdData(data, "image_labeling")
-    cd.createTask("data", presenter, "task_id").getTaskResult("task_id", "results")
+    data = ['http://farm4.static.flickr.com/3114/2524849923_1c191ef42e.jpg', 'http://www.7-star-admiral.com/0015_animals/0629_angora_hamster_clipart.jpg']
+    cd = CrowdData('http://localhost:7000/', '8df67fd6-9c9b-4d32-a6ab-b0b5175aba30', data, "presenter.html", "test3", "test3", "test3")
+    cd.createTask(n_answers = 1).getTaskResult()
+    print cd.cd["task_col"]
+    print cd.cd["result_col"]

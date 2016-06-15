abe7f1cdfc734b75c79d323299e0fa2aa6af2dc1
Jiannan Wang <jnwangir@gmail.com>
2016-06-14 23:32:39 -0700

    add comments to crowddata.py

--- crowddata.py
+++ crowddata.py
@@ -13,76 +13,85 @@ class CrowdData:
         self.cd = {'data':data, 'id_list':id_list}
         self.flag = ["data", "id_list"]
         self.projects_presenter = {}
-        self.task_presenter = {}
+        self.task_presenter = {} 
+
+        # JN: I think we should require the user to input a presenter object (rather than a string). We can infer the class name of the object and use self.cd_name + the class name as the project name. Otherwise, the user will not be able to use a customized presenter 
         presenter = ["img", "video", "sound"]
         for p in presenter:
             self.projects_presenter[p] = -1
+
         self.endpoint = endpoint
         pbclient.set('endpoint', endpoint)
         pbclient.set('api_key', api_key)
         self.cddb = sqlite3.connect('crowddata.db')
         self.cur = self.cddb.cursor()
         exe_str = "CREATE TABLE " + self.cd_name + " (id integer, col_name varchar(100), value text DEFAULT NULL, PRIMARY KEY(id, col_name))"
+        # jn: what is the table has already existed in the database?
         self.cur.execute(exe_str)
 
 
+                         # jn: see my previous comment on the presenter. 
     def createTask(self, presenter, input_col = "data", output_col = "task", n_answers = 30, priority_0 = 0, quorum = 0):
         if input_col not in self.flag:
-            print "input error1"
+            print "input error1" #jn: need to return a more clear error info. We can work on this later.
             return
         if output_col in self.flag:
-            print "output already exisits"
+            print "output already exisits" #jn: need to return a more clear error info. We can work on this later
             output_col = output_col + "_1"
-            f = 1
-        else:
-            f = 0
         self.flag.append(output_col)
         if self.projects_presenter[presenter] == -1:
             p = pbclient.create_project(self.cd_name + "_" + presenter, self.short_name + "_" + presenter, self.description + "_" + presenter)
             self.projects_presenter[presenter] = p.id
             p.info['task_presenter'] = open("presenter/" + presenter + ".html").read()
             pbclient.update_project(p)
-        self.cd[output_col] = []
-        for d in self.cd[input_col]:
-            if f == 0:
-                exe_str = "SELECT * FROM " + self.cd_name + " WHERE id=? AND col_name=?"
-                self.cur.execute(exe_str, (self.cd['id_list'][self.cd[input_col].index(d)], output_col, ))
-                data = self.cur.fetchall()
-                if data != []:
-                    self.cd[output_col].append(eval(data[0][2]))
-                    continue
+
+        self.cd[output_col] = [None] * len(self.cd[input_col])
+        assert len(self.cd[id_list] == len(self.cd[input_col])
+        for k, (i, d) in enumerate(zip(self.cd[id_list], self.cd[input_col])):
+            exe_str = "SELECT * FROM " + self.cd_name + " WHERE id=? AND col_name=?"
+            self.cur.execute(exe_str, (i, output_col, ))
+            data = self.cur.fetchall()
+            if data != []:
+                assert len(data) == 1
+                self.cd[output_col].append(eval(data[0][2]))
+                continue
+
             task_info = {'url_b': d}
             task = pbclient.create_task(self.projects_presenter[presenter], task_info, n_answers, priority_0, quorum)
-            exe_str = "INSERT INTO " + self.cd_name + " VALUES(?,?,?)"
-            self.cur.execute(exe_str, (self.cd['id_list'][self.cd[input_col].index(d)], output_col, str(vars(task)['data']), ))
-            self.cd[output_col].append(vars(task)['data'])
+            self.cur.execute(exe_str, (i, output_col, str(vars(task)['data']), ))
+            self.cd[output_col][k] = vars(task)['data']
             self.task_presenter[str(task.id)] = presenter
+
         return self
 
     def getTaskResult(self, input_col = "task", output_col = "result"):
         if input_col not in self.flag:
-            print "input error2"
+            print "input error2"  #see my previous comment
             return
         if output_col in self.flag:
-            print "output already exisits"
+            print "output already exisits" #see my previous commemt
             output_col = output_col + "_1"
-            f = 1
-        else:
-            f = 0
         self.flag.append(output_col)
-        self.cd[output_col] = []
-        for d in self.cd[input_col]:
-            if f == 0:
+        self.cd[output_col] = [None] * len(self.cd[input_col])
+
+        assert len(self.cd[id_list] == len(self.cd[input_col])
+
+        # I revised the following code to make sure every completed task can be stored in the self.cd right away
+        rn = 0
+        while rn < len(self.cd[input_col]):
+            for k, (i, d) in enumerate(zip(self.cd[id_list], self.cd[input_col])):
                 exe_str = "SELECT * FROM " + self.cd_name + " WHERE id=? AND col_name=?"
-                self.cur.execute(exe_str, (self.cd['id_list'][self.cd[input_col].index(d)], output_col, ))
+                self.cur.execute(exe_str, (i, output_col, ))
                 data = self.cur.fetchall()
                 if data != []:
+                    assert len(data) == 1
                     self.cd[output_col].append(eval(data[0][2]))
                     continue
-            url = self.endpoint + "project/" + self.cd_name + "_" + self.task_presenter[str(d['id'])] + "/" + str(d['id']) + "/results.json"
-            print url
-            number = 0
-            while number < d['n_answers']:
+
+                # jn: this part does not look very elegant. Does pbclient provides an API that can return the return based on a given task id? If not, would be good to consider how to add one. 
+                url = self.endpoint + "project/" + self.cd_name + "_" + self.task_presenter[str(d['id'])] + "/" + str(d['id']) + "/results.json"
+                print url
+
                 result = requests.get(url)
                 if result.status_code == 200:
                     result_json = result.json()
@@ -90,10 +99,13 @@ class CrowdData:
                     print result_json
                     print number
                     print d['n_answers']
-                time.sleep(60)
-            exe_str = "INSERT INTO " + self.cd_name + " VALUES(?,?,?)"
-            self.cur.execute(exe_str, (self.cd['id_list'][self.cd[input_col].index(d)], output_col, str(result_json)))
-            self.cd[output_col].append(result_json)
+                assert number <= d['n_answers']
+                if number == d['n_answers']:
+                    rn += 1
+                    exe_str = "INSERT INTO " + self.cd_name + " VALUES(?,?,?)"
+                    self.cur.execute(exe_str, (i, output_col, str(result_json)))
+                    self.cd[output_col][k] = result_json
+            time.sleep(60)
         return self
 
     def __del__(self):

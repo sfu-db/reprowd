04d8836a32d9d3af9a384e64bed03e1f876a8a7f
Morphiling <ruochenj@sfu.ca>
2016-07-27 18:56:05 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='crowdbase',
     version='1.0.0',
-    packages=find_packages(),
+    packages=find_packages(exclude=['*.docs','*.tests','*.exmaples']),
     author='SFU DB LABS',
     description='crowdbase is a library to make crowdsourcing more convenient',
     author_email='jnwang@sfu.ca',
